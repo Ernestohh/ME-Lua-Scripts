6e71c2aa40ebf60dd8840819d5b4e469f1677bde
Ernie <ernestohh97@gmail.com>
2025-02-26 09:58:25 +0100

    Update Ernie-KerapacBosser.lua

--- Ernie-KerapacBosser.lua
+++ Ernie-KerapacBosser.lua
@@ -1,4 +1,4 @@
-local version = "1"
+local version = "1.2"
 local API = require("api")
 API.SetDrawLogs(true)
 
@@ -40,7 +40,7 @@ local passiveBuffs = {
 }
 
 local foodItems = { "Sailfish", "Rocktail", "Sailfish soup", "Desert sole", "Ghostly sole", "Beltfish", "Catfish" }
-local superRestoreItems = { "Super restore potion (4)", "Super restore potion (3)", "Super restore potion (2)", "Super restore potion (1)", "Super restore flask (6)", "Super restore flask (5)", "Super restore flask (4)", "Super restore flask (3)", "Super restore flask (2)", "Super restore flask (1)" }
+local superRestoreItems = { "Super restore (4)", "Super restore (3)", "Super restore (2)", "Super restore (1)", "Super restore flask (6)", "Super restore flask (5)", "Super restore flask (4)", "Super restore flask (3)", "Super restore flask (2)", "Super restore flask (1)" }
 
 local bossStateEnum = {
     BASIC_ATTACK = { name = "BASIC_ATTACK", animations = { 34192 } },
